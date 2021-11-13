e29c45ced5cfab9725bd912266a01b75f985dd4f
Qualyn Margain <Qualynmargain@gmail.com>
2021-11-12 16:10:18 -0800

    done

--- index.js
+++ index.js
@@ -28,12 +28,40 @@ myFunction();
     2. Use a counter to return the summation of that number. 
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
-function summation (number){
+
+function summation(){}
+// let number =0;
+// for(let i =0; i <100; i++){
+//   number[i]= i++;
+// }
+// }
+
+
+
+// function summation (number){
+//   let sum = 0;
+//   for(let i =0; i < number; i++){
+//     sum += summation(number[i], 10);
+//   }
+// }
+
+
+
+
+
+// function summation (number){
+//   let result = 0;
+//   for(let i=1; i<=number; i++)
+//   result += summation(i);
+//   return result;
+// }
+
+
+
 //   let num = [1, 2, 3, 4, 5 ,6].reduce(function(num,item){
 //  for(let i = 0; i < number; i ++){
 //    return num + item;
 //  },0);
-}
 //  console.log(summation[4]);
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
@@ -135,11 +163,14 @@ const zooAnimals = [
   */
 
   function USApop(data){
-    let populationTotal = data.reduce((accumulator, currentValue) =>{
-      return accumulator += currentValue
-    },0);
-    console.log(USApop(populationTotal(zooAnimals)));
+    let populationTotal = data.reduce(function(accumulator, item){ 
+      return accumulator + item.population;
+    }, 0)
+    return populationTotal;
   }
+     
+    console.log('task 4',USApop(zooAnimals))
+  
   
   
   
@@ -151,29 +182,32 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(){
+  function consume(a, b, cb){
+    return numberCB (a,b);
+    // return `${a} + ${b} = ${cb}`;
   }
+
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(a,b){
+    return a + b;
   }
 
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
+function multiply(a,b){
+   return a*b;
   }
 
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+function greeting(a,b){
+   return `Hello ${a} ${b}, nice to meet you!`;
   }
   
   
@@ -188,21 +222,21 @@ function greeting(/*Your Code Here */){
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(length, width, height){
- this.length = length;
- this.width = width;
- this.height = height;
+function CuboidMaker(obj){
+ this.length =obj.length;
+ this.width = obj.width;
+ this.height = obj.height;
 }
-CuboidMaker.prototype.length = function (){
 
-}
 
 
 /* 🐴🐴🐴 Step 2: Volume Method 🐴🐴🐴
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 
-
+CuboidMaker.prototype.volume = function(){
+  return this.length * this.width * this.height;
+}
 
 
 
@@ -211,7 +245,9 @@ CuboidMaker.prototype.length = function (){
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 
-
+CuboidMaker.prototype.surfaceArea = function(){
+  return 2*(this.length*this.width) + (this.length*this.height)+(this.width*this.height);
+}
 
 
 
@@ -219,7 +255,11 @@ CuboidMaker.prototype.length = function (){
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
+const cuboid = new CuboidMaker ({
+  length: 4,
+  width: 5,
+  height:5,
+});
 
 
 
@@ -232,7 +272,17 @@ CuboidMaker.prototype.length = function (){
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+constructor(cuboidTwo){
+  this.length = cuboidTwo.length;
+  this.width = cuboidTwo.width;
+  this.height =cuboidTwo.height;
+}
+volume(){
+  return this.length*this.width*this.height;
+}
+surfaceArea(){
+  return 2*(this.length*this.width) + (this.length*this.height)+(this.width*this.height);
+}
 }
 
 
